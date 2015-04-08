1bff44bac1f8c83a6219ee38dda588ee2e3ac265
Jee Vang <vangjee@gmail.com>
2015-04-08 07:14:30 -0400

    - add companion project to learn scala/spark

---
+++ learn-scala/src/main/scala/learn/scala/PearsonCorrelationApp.scala
@@ -0,0 +1,66 @@
+package learn.scala
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+case class Index(i:Integer, j:Integer)
+case class Computation(x:Double,y:Double,xx:Double, yy:Double, xy:Double, n:Double)
+
+object PearsonCorrelationApp {
+  def main(args:Array[String]) {
+    val input = args(0)
+    val output = args(1)
+    
+    val conf = new SparkConf()
+     .setAppName(s"Pearson Correlation $input")
+    val sc = new SparkContext(conf)
+    
+    val file = sc.textFile(input)
+    val tmap = file.flatMap(line => {
+      val arr = line.split(",")
+      for {
+        i <- 0 until arr.length
+        j <- (i+1) until arr.length
+      } yield {
+        val x = arr(i).toDouble
+        val y = arr(j).toDouble
+        val xx = x * x
+        val yy = y * y
+        val xy = x * y
+        val k = new Index(i, j)
+        val v = new Computation(x, y, xx, yy, xy, 1.0d)
+        (k, v)
+      }
+    })
+    
+    val tred = tmap.reduceByKey((a:Computation, b:Computation) => {
+      val x = a.x + b.x
+      val y = a.y + b.y
+      val xx = a.xx + b.xx
+      val yy = a.yy + b.yy
+      val xy = a.xy + b.xy
+      val n = a.n + b.n
+      val c = new Computation(x, y, xx, yy, xy, n)
+      (c)
+    })
+    
+    tred.map(f => { 
+      val k = f._1
+      val v = f._2
+      
+      val num = v.xy - (v.x * v.y / v.n)
+      val den1 = (v.xx - (math.pow(v.x, 2.0d)/v.n))
+      val den2 = (v.yy - (math.pow(v.y, 2.0d)/v.n))
+      val den3 = den1 * den2
+      val den = math.sqrt(den3)
+      val r = num / den
+      val i = k.i
+      val j = k.j
+      val soutput = s"$i, $j, $r"
+      println(soutput)
+      (soutput)
+    }).saveAsTextFile(output)
+    
+    
+  }
+}
---
+++ learn-scala/tutorials/run-countkeyword.md
@@ -0,0 +1,35 @@
+A quick tutorial on using the count keyword application.
+========================================================
+
+# Make a data directory in HDFS.
+```
+hdfs dfs -mkdir /data
+```
+# Copy the /tutorials/data/test.text to HDFS.
+```
+hdfs dfs -copyFromLocal test.text /data
+```
+
+Note the contents of this text files comes from
+[Micromegas by Voltaire](https://www.gutenberg.org/ebooks/30123).
+
+# Run the count keyword application on YARN.
+```
+$SPARK_HOME/bin/spark-submit --class learn.scala.CountKeywordApp \
+    --master yarn \
+    --num-executors 10 \
+    --executor-cores 2 \
+    scala-0.0.1-SNAPSHOT.jar \
+    /data/test.txt \
+    the
+```
+# Run the count keyword application on Spark.
+```    
+$SPARK_HOME/bin/spark-submit --class learn.scala.CountKeywordApp \
+    --master spark://node1:7077 \
+    --num-executors 10 \
+    --executor-cores 2 \
+    scala-0.0.1-SNAPSHOT.jar \
+    /data/test.txt \
+    the
+```
